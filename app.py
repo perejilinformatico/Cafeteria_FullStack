330e7b12abe1ac1fc2958d62f3e27fcf7cc10eb6
Santiago cussi <perejilinformatico@gmail.com>
2025-12-13 11:39:55 +0100

    Modificacion del css, html y js

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
@@ -18,18 +19,21 @@ def enviado():
     if request.method == 'POST':
         nombre = request.form.get('nombre')
         cafe_seleccionado = request.form.get('Cafes')
+        fecha = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         pedido = {
             'nombre': nombre,
             'cafe': cafe_seleccionado,
+            'fecha': fecha,
         }
         pedidos.append(pedido)
-        return redirect(url_for('cafe_enviado_user', nombre=nombre, cafe_seleccionado=cafe_seleccionado))
+        return redirect(url_for('cafe_enviado_user', nombre=nombre, cafe_seleccionado=cafe_seleccionado, fecha=fecha))
 
 @app.route('/cafe_enviado_user')
 def cafe_enviado_user():
     nombre = request.args.get('nombre')
     cafe_seleccionado = request.args.get('cafe_seleccionado')
-    return render_template('cafe_enviado_user.html', nombre=nombre, cafe_seleccionado=cafe_seleccionado)
+    fecha = request.args.get('fecha')
+    return render_template('cafe_enviado_user.html', nombre=nombre, cafe_seleccionado=cafe_seleccionado, fecha=fecha)
 
 @app.route('/admin')
 def admin():
